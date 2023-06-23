d96b04a244b18698cc2362dbc140312acb9836b8
DJ Judd <dj.judd@gmail.com>
2023-06-23 02:09:32 -0500

    player value is now passed between routes

--- madlibs.py
+++ madlibs.py
@@ -1,12 +1,8 @@
-"""A madlib game that compliments its users."""
-
+from flask import Flask, render_template, request, session
 from random import choice
 
-from flask import Flask, render_template, request
-
-# "__name__" is a special Python variable for the name of the current module.
-# Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
+app.secret_key = "secret-key" # replace with your own secret key
 
 AWESOMENESS = [
     "awesome",
@@ -25,59 +21,43 @@ AWESOMENESS = [
     "lovely",
 ]
 
-
-@app.route("/index")
+@app.route("/")
 def landing_page():
     """Display homepage."""
-
     return render_template("index.html")
-
 
 @app.route("/hello")
 def say_hello():
     """Say hello to user."""
-
     return render_template("hello.html")
-
 
 @app.route("/greet")
 def greet_person():
     """Greet user with compliment."""
-
-    player = request.args.get("person")
-
+    session['player'] = request.args.get("player")  # Store player's name in session
     compliment = choice(AWESOMENESS)
-
-    return render_template("compliment.html", person=player, compliment=compliment)
+    return render_template("compliment.html", player=session['player'], compliment=compliment)
 
 @app.route("/game")
 def show_madlib_form():
-
     wants_to_play_game = request.args.get("RadioSelection")
-    
     if wants_to_play_game == "yes":
-        return render_template("game.html")
-
+        return render_template("game.html", player=session['player'])  # Fetch player's name from session
     else:
-        # go to a page called goodbye.html
-        return render_template("goodbye.html")
+        player_name = session.get('player')  # Get the player's name before removing it
+        session.pop('player', None)  # Remove player from session
+        return render_template("goodbye.html", player=player_name)
 
 @app.route("/madlib")
 def show_madlib(): 
     color = request.args.get("color")
     noun = request.args.get("noun")
-    name = request.args.get("name")
     adjective = request.args.get("adjective")
-
     return render_template("madlib.html",
-                           color = color,
-                           noun = noun,
-                           name = name,
-                           adjective = adjective)
-
+                           color=color,
+                           noun=noun,
+                           name=session['player'],  # Fetch player's name from session
+                           adjective=adjective)
 
 if __name__ == "__main__":
-    # Setting debug=True gives us error messages in the browser and also
-    # "reloads" our web app if we change the code.
-
     app.run(debug=True, host="0.0.0.0")
